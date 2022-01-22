5ae85e109e3f274d3357159931855ec8d2d12c76
Nallan :3 <96462575+rbbyptr4@users.noreply.github.com>
2022-01-22 11:38:19 +0700

    Add files via upload

---
+++ FunctionJS-Array.js
@@ -0,0 +1,68 @@
+// //  Function toString()
+// const Heroes = ['Ling', ' Yin', ' Lancelot', ' Selena'];
+//     console.log(Heroes.toString());
+
+// Function join()
+// const Heroes = ['Ling', 'Yin', 'Lancelot', 'Selena'];
+//     console.log(Heroes.join(' , '));
+
+//  Function pop()
+// const Heroes = ['Ling', 'Yin', 'Lancelot', 'Selena'];
+//     console.log(Heroes.pop());
+// let Hero = Heroes.pop()
+//     console.log(Hero);
+//     console.log(Heroes.pop());
+//     console.log(Heroes.pop());
+
+//  Function push()
+// const Heroes = ['Ling', 'Yin', 'Lancelot', 'Selena'];
+//     let length = Heroes.push('Xavier')
+//     console.log(Heroes);
+
+//  Function shift()
+// const Heroes = ['Ling', 'Yin', 'Lancelot', 'Selena'];
+//     console.log(Heroes.shift());
+//     let Hero = [Heroes.shift()]
+//     console.log(Hero);
+
+//  Function unshift()
+// const Heroes = ['Ling', 'Yin', 'Lancelot', 'Selena'];
+//     console.log(Heroes.unshift('Xavier'));
+//     console.log(Heroes);
+
+//  Function Change Elements Index
+// const Heroes = ['Ling', 'Yin', 'Lancelot', 'Selena'];
+//     console.log(Heroes[2] = 'Xavier');
+//     console.log(Heroes);
+
+//  Function Array Length
+// const Heroes = ['Ling', 'Yin', 'Lancelot', 'Selena'];
+//     console.log(Heroes[Heroes.length] = 'Xavier');
+//     console.log(Heroes);
+
+//  Function Delete
+// const Heroes = ['Ling', 'Yin', 'Lancelot', 'Selena'];
+//     delete Heroes[0];
+//     console.log(Heroes);
+
+//  Function Merge
+// const Assassin = ['Ling', 'Helcurt'];
+// const Mage = ['Xavier' , 'Odette'];
+// const Marksman = ['Miya' , 'Granger']
+
+// const Heroes = Assassin.concat(Mage, Marksman)
+// console.log(Heroes);
+
+// const assHeroes = Assassin.concat('Yi Sun-shin')
+// console.log(assHeroes);
+
+//  Function Splice n Slice
+// const Heroes = ['Ling', 'Yin', 'Lancelot', 'Selena'];
+//     Heroes.splice(2 , 0, 'Xavier' , 'Karina')
+//     console.log(Heroes);
+//     Heroes.splice(0, 2 )
+//     console.log(Heroes);
+
+// const splitHero = Heroes.slice(1, 3);
+//     console.log(splitHero);
+//     console.log(Heroes);
---
+++ JavaScript-Test.js
@@ -0,0 +1,99 @@
+// -----------------------------
+//  1. Fuction Shoping Program
+// -----------------------------
+
+// var Ask = true
+// while ( Ask ) {
+// var Price = 20000;
+// var myMoney = prompt ('Harga Produk ini Sebesar : ' + Price + '\n\n' + 'Ketik Disini Untuk Mengecek Uangmu : \nContoh : (5000 , 10000)');
+// var Discount = 5000;
+// var Change = myMoney - Price;
+// var Result = '';
+
+// if ( myMoney == Price ) {
+//     Result = 'Uangmu Pas, Tidak Ada Kembalian'
+// }   else if ( myMoney + Discount <= Price ) {
+//     Result = 'Selamat Anda Mendapatkan Diskon Sebesar Rp.${Discount}';
+// }   else if ( myMoney > Price ) {
+//     Result = 'Kembalianmu Sebesar : ' + Change;
+// }   else if ( myMoney <= Price ) {
+//     Result = confirm ('Uangmu Kurang, Yakin Tidak Ingin Menggunakan Diskon?') 
+//         if ( myMoney + Discount == Price) {
+//         Result = 'Uangmu Kurang Untuk Membeli Produk Ini, Yakin Tidak Ingin Menggunakan Diskon?';
+//         } else {
+//         Result = confirm ( 'Selamat Anda Mendapatkan Diskon Sebesar : ' + Discount);
+//         }
+// } else if ( myMoney < Price ) {
+//     Result = 'Uangmu Kurang, Sebaiknya Gunakan Diskon Untuk Menghemat Uangmu'
+// } 
+
+// alert('Uangmu Sebesar : ' + myMoney + ' Dan Harga Barang Tersebut Sebesar : ' + Price + ' ' + Result );
+
+// Ask = confirm ('Ingin Berbelanja Lagi?');
+
+// }
+
+// alert ('Terimakasih Telah Berbelanja Disini')
+
+// ------------------------------
+// 2. Function Count Round Area
+// ------------------------------
+
+// function roundArea (jaringJaring) {
+//     console.log((jaringJaring * jaringJaring) * 3.14);
+// } 
+
+// roundArea ( 28 );
+// roundArea ( 14 );
+// roundArea ( 32 );
+
+// ---------------------------------------------------------
+// 3. Function Convert Hours to Minute and Minute to Second
+// ----------------------------------------------------------
+
+//  Hours to Minutes
+// function convertHours (Hours) {
+//     console.log(Hours * 60 + " Minutes");
+// }
+// convertHours (5)
+// convertHours (3)
+// convertHours (2)
+
+// Minutes to Second
+// function convertMinutes (Minutes) {
+//     console.log(Minutes * 60 + " Second");
+// }
+// convertMinutes (5)
+// convertMinutes (3)
+// convertMinutes (2)
+
+// ----------------
+// 4. Even or Od
+// ----------------
+
+// function evenOrod(arr) {
+//     if (arr % 2 == 0) {
+//         console.log("Even");
+//     }
+//     else {
+//         console.log("Odd");
+//     }
+// }
+
+// evenOrod ([0])
+// evenOrod ([1])
+// evenOrod ([])
+// evenOrod ([0, 1, 5])
+
+// ----------------
+// 5. Looping For
+// ----------------
+
+// var s = '';
+// for ( var i = 10; i > 0; i--) {
+//     for ( var j = 0; j < i; j++) {
+//         s += Math.random()
+//     }
+//         s += '\n';
+//     }
+// console.log(s);

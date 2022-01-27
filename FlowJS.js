f82cde496285efec8ae5df20ee0d101f2ed57686
Nallan :3 <96462575+rbbyptr4@users.noreply.github.com>
2022-01-27 13:38:37 +0700

    Add files via upload

---
+++ FlowJS.js
@@ -0,0 +1,33 @@
+// // Count Area Triangle
+// function areaTriangle (Alas, Tinggi) {
+//     console.log(0.5 * Alas * Tinggi);
+// }
+
+// areaTriangle (8, 10);
+// areaTriangle (5, 7);
+// areaTriangle (9, 2);
+
+// // Less Than 100
+// function lessThan100 (a, b) { 
+// if (a + b  < 100) {
+//    console.log('Less Than 100'); 
+// } else {
+//     console.log('More Then 100');
+// }
+// }
+
+// lessThan100(22, 15);
+// lessThan100(83, 34);
+// lessThan100(3, 77);
+
+// // Count by Characters
+// function comp (str1, str2) {
+//     if (str1.length == str2.length) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+// comp("AB", "CD")
+// comp("ABC", "DE") 
+// comp("hello", "edabit")
